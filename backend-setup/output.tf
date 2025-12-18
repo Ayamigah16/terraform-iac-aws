c26b496207ae2fb6eec629a65485b48c5d34ffb5
Abraham Ayamigah <abkc070@gmail.com>
2025-12-18 11:46:02 +0000

    Updated backend setup output

--- backend-setup/output.tf
+++ backend-setup/output.tf
@@ -1,10 +1,14 @@
 output "s3_bucket_name" {
-  description = "S3 bucket used"
+  description = "S3 bucket used for Terraform state"
   value       = aws_s3_bucket.state_bucket.bucket
 }
-
 
 output "dynamodb_table_name" {
   description = "DynamoDB table used for state locking"
   value       = aws_dynamodb_table.state_lock_table.name
+}
+
+output "aws_region" {
+  description = "AWS region where backend resources are deployed"
+  value       = var.aws_region
 }
